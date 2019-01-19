e463ef7af905d7f5b6c887ea1944e238ccbcee52
antlammi <antti.lammi@helsinki.fi>
2019-01-19 18:22:14 +0200

    6,7,8,9,10

--- osa1/HelloWorld/src/index.js
+++ osa1/HelloWorld/src/index.js
@@ -1,15 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+
+const App = (props) => { 
+    const [clicks, setClicks] = useState({
+        left:0, right: 0
+    })
+    const [allClicks, setAll] = useState([])
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setClicks({
+            ...clicks,
+            left: clicks.left +1
+        })
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setClicks({
+            ...clicks,
+            right: clicks.right +1
+        })
+    }
+    const History = (props) => {
+        if (props.allClicks.length == 0) {
+            return(
+                <div>
+                    sovellusta käytetään nappeja painelemalla
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                näppäilyhistoria: {props.allClicks.join(' ')}
+            </div>
+        )
+    }
+
+    const Button = ({handleClick, text}) => (
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    )
     return (
         <div> 
-            <p>Hello {props.name}, you are {props.age} years old</p>
+            <div>
+                {clicks.left}
+                <Button handleClick={handleLeftClick} text='vasen'/>
+                <Button handleClick={handleRightClick} text='oikea'/>
+                {clicks.right}
+                <History allClicks = {allClicks}/>
+            </div>
         </div>
     )
 }
+/*const App = (props) => {
+    const [ counter, setCounter ] = useState(0)
+    const setToValue = (value) => setCounter(value)
+    
 
-const App = () => {
+    return(
+
+        <div>
+            <Display counter={counter}/>
+            <Button handleClick={() => setToValue(counter+1)} text='plus'/>
+            <Button handleClick={() =>setToValue(0)} text='zero'/>
+           
+        </div>
+    )
+}
+*/
+
+
+/*const App = () => {
     const nimi = 'Jupu'
     const ika = 12
     return (
@@ -20,5 +84,5 @@ const App = () => {
     </div>
     )
 }
-
+*/
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+const Statistic = ({name, value}) => (
+    <p>{name} {value}</p>
+)
+
+const Statistics = (props) => {
+    let count = props.good + props.neutral + props.bad
+    if (count===0){
+        return(
+            <div>
+                <h1>statistiikka</h1>
+                <p>Ei yhtään palautetta annettu</p>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <h1>statistiikka</h1>
+            <Statistic name='hyvä' value={props.good}/>
+            <Statistic name='neutraali' value={props.neutral}/>
+            <Statistic name='huono' value={props.bad}/>
+            <Statistic name='yhteensä' value={count}/>
+            <Statistic name='average' value={
+                <Average good={props.good} bad={props.bad} count={count}/>
+            }/>
+            <Statistic name='positiivisia' value={
+                <Positive good={props.good} count ={count}/>
+            }/>
+        </div>
+    )
+}
+const Average = ({good, bad, count}) => {
+    let value = good-bad
+    return (value/count)
+}
+const Positive = ({good, count}) => {
+    return (100*(good/count)+ '%')
+}
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  return (
+    <div>
+        <h1>anna palautetta</h1>
+        <Button handleClick={() => setGood(good+1)} text='hyvä'/>
+        <Button handleClick={() => setNeutral(neutral+1)} text='neutraali'/>
+        <Button handleClick={() => setBad(bad+1)} text='huono'/>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
