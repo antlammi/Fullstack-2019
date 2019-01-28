649450d5bd29b96eb120fc2f5c716e0116dbe10e
antlammi <antti.lammi@helsinki.fi>
2019-01-28 21:53:58 +0200

    11,15,16,17

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
+
 const Person = (props) => {
   return(
   <div>
-    <li>{props.name} {props.number}</li>
+    <li>{props.name} {props.number} <button onClick={(() => 
+      props.removePerson(props.id))}>poista</button></li>
   </div>
   )
 }
@@ -34,27 +37,29 @@ const PersonForm = (props) => {
   )
 }
 
-const Persons = ({personsToShow}) => {
+const Persons = ({personsToShow, removePerson}) => {
   return(
     <div>
       <h2>Numerot</h2>
       <ul>
-        {personsToShow.map(person => <Person key={person.name} name={person.name} number={person.number}/>)}
+        {personsToShow.map(person => <Person key={person.id} name={person.name} number={person.number} removePerson={removePerson} id={person.id}/>)}
       </ul>
     </div>
   )
 }
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Martti Tienari', number: '040-123456' },
-    { name: 'Arto Järvinen', number: '040-123456' },
-    { name: 'Lea Kutvonen', number: '040-123456' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newShowCriteria, setNewShowCriteria] = useState('')
   const [showAll, setShowAll] = useState(true)
+ 
+  useEffect(()=> {
+    personService.getAll()
+    .then(initialPersons => {
+      setPersons(initialPersons)
+    })
+  }, [])
 
   const personsToShow = showAll 
   ? persons 
@@ -71,12 +76,25 @@ const App = () => {
     if (persons.map(person=> person.name).includes(newName)){
       window.alert(`${newName} on jo luettelossa`)
     } else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      personService
+      .create(personObject)
+      .then(returnedPerson=> {
+        console.log(returnedPerson)
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   }
-  
+  const removePerson = id => {
+    console.log(id)
+    const person = persons.find(n => n.id === id)
+    console.log(person)
+    if (window.confirm(`Poistetaanko ${person.name}?`)){
+      personService.remove(id)
+      .then(setPersons(persons.filter(person => person.id !== id)))
+      }
+  }
   const nameListener = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -101,7 +119,7 @@ const App = () => {
         newName={newName} nameListener={nameListener} 
         newNumber={newNumber} numberListener={numberListener}
       />
-     <Persons personsToShow={personsToShow}/>
+     <Persons personsToShow={personsToShow} removePerson={removePerson}/>
       
     </div>
   )
