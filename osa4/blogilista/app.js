5a7016b0c1b852a8eeebd8cdaa2bca4f391ad0c4
antlammi <antti.lammi@helsinki.fi>
2019-02-18 02:25:19 +0200

    4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogSchema = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+
+const Blog = blogSchema
+app.use('/api/blogs', blogsRouter)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true})
+
+app.use(cors())
+app.use(bodyParser.json())
+
+module.exports = app
